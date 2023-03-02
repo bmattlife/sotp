e761ade9869f21c33ddb3468e94fb4503338a100
Brian Cohen <5685887+bmattlife@users.noreply.github.com>
2023-03-01 16:49:46 -0800

    add 'issuer' cmd-line arg

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use clap::Parser;
 
 /// A simple time-based one-time password generator
-#[derive(Parser)]
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct Config {
     /// The issuer of the key
-    issuer: String,
+    issuer: Option<String>,
 }
 
 fn main() {
-    let _args = Config::parse();
+    let args = Config::parse();
+    dbg!(args);
 }
