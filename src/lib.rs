718405fde66fcbd30cc19898021a85a1072fbc5b
Brian Cohen <5685887+bmattlife@users.noreply.github.com>
2023-03-30 19:29:59 -0700

    move code generation from main() into lib.rs. Code generation seems to be broken

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,62 @@
 pub mod otp {
-    pub fn gen_otp(_secret: &str) -> &str {
-        "123456"
+
+    use std::time::SystemTime;
+
+    use hmac::{Hmac, Mac};
+    use sha1::Sha1;
+
+    type HmacSha1 = Hmac<Sha1>;
+
+    #[derive(Debug)]
+    pub enum Algorithm {
+        SHA1,
+        SHA256,
+        SHA512,
+    }
+
+    #[derive(Debug)]
+    pub struct Totp {
+        secret: String,
+        issuer: Option<String>,
+        account_name: Option<String>,
+        algorithm: Option<Algorithm>,
+        digits: Option<u32>,
+        period: Option<u64>,
+    }
+
+    impl Totp {
+        pub fn new(secret: String) -> Self {
+            Self {
+                secret: secret,
+                issuer: None,
+                account_name: None,
+                algorithm: None,
+                digits: None,
+                period: None,
+            }
+        }
+
+        fn get_timesteps(&self) -> u64 {
+            let period = self.period.unwrap_or(30);
+            let unix_time_secs = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+
+            unix_time_secs / period
+        }
+
+        pub fn get(&self) -> String {
+            let mut mac = HmacSha1::new_from_slice(self.secret.as_bytes()).unwrap();
+            mac.update(&self.get_timesteps().to_be_bytes());
+
+            let result = mac.finalize().into_bytes();
+
+            let offset = (result.last().unwrap() & 15) as usize;
+            let bin_code =
+                u32::from_be_bytes(result[offset..offset + 4].try_into().unwrap()) & 0x7fff_ffff;
+            let totp = bin_code % 10_u32.pow(self.digits.unwrap_or(6));
+            totp.to_string()
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
+use base32;
 use clap::Parser;
-use hmac::{Hmac, Mac};
-use sha1::Sha1;
-use std::time::SystemTime;
-type HmacSha1 = Hmac<Sha1>;
-use totp_rs::{Algorithm, Secret, TOTP};
+use sotp::otp::Totp;
 
 /// A simple time-based one-time password generator
 #[derive(Parser, Debug)]
@@ -15,33 +12,11 @@ struct Config {
 
 fn main() {
     let args = Config::parse();
-    // Generate hmac-sha-1 for key and timestep
-    let secret = args.issuer.unwrap();
-    let mut mac = HmacSha1::new_from_slice(&secret.as_bytes()).unwrap();
-
-    let unix_time_secs = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-
-    let time_steps = unix_time_secs / 30;
-    mac.update(&time_steps.to_be_bytes());
-    let result = mac.finalize().into_bytes();
-
-    // dynamic truncation
-    let offset = (result.last().unwrap() & 15) as usize;
-    let bin_code = u32::from_be_bytes(result[offset..offset + 4].try_into().unwrap()) & 0x7fff_ffff;
-    let totp = format!("{1:00$}", 6, bin_code % 10_u32.pow(6));
-    dbg!(totp);
-
-    let totp = TOTP::new(
-        Algorithm::SHA1,
-        6,
-        1,
-        30,
-        Secret::Raw(secret.as_bytes().to_vec()).to_bytes().unwrap(),
-    )
-    .unwrap();
-    let token = totp.generate_current().unwrap();
-    println!("{}", token);
+    /*let encoded = base32::encode(
+        base32::Alphabet::RFC4648 { padding: true },
+        args.issuer.unwrap().as_bytes(),
+    );*/
+    let totp = Totp::new(args.issuer.unwrap());
+    dbg!(&totp);
+    dbg!(&totp.get());
 }
